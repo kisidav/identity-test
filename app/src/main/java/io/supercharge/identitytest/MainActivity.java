9093b4e87e3155cdd68363b8b5a8a31f2236f4ad
David Kisida <david.kisida@supercharge.io>
2018-08-08 11:27:16 +0200

    Add test project

---
+++ app/src/main/java/io/supercharge/identitytest/MainActivity.java
@@ -0,0 +1,43 @@
+package io.supercharge.identitytest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import de.identity.identityvideo.sdk.IdentityVideoSDK;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> IdentityVideoSDK.getInstance().startIdentification(this, 1234, ""));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/io/supercharge/identitytest/TestApplication.java
@@ -0,0 +1,16 @@
+package io.supercharge.identitytest;
+
+import android.app.Application;
+
+import de.identity.identityvideo.sdk.IdentityVideoSDK;
+
+public class TestApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        IdentityVideoSDK.initialize(this);
+        IdentityVideoSDK.setMode(IdentityVideoSDK.Mode.TEST);
+    }
+}
